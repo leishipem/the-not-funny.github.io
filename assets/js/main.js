ad095bb96ad65ba513322195774e7c0b92358e20
Leishipem <leishipem8@gmail.com>
2024-02-13 22:04:33 +0530

    footer social animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,9 +16,9 @@ const sr = ScrollReveal({
     reset:true
 })
 
-sr.reveal(`.home__img, .gigs__data, .footer__content`, { delay: 500 })
+sr.reveal(`.home__img, .gigs__data, .footer__content, .footer__social`, { delay: 500 })
 sr.reveal(`.booking-container`, { delay: 500 })
 
-sr.reveal(`.home__social, .footer__social, .footer__create`, { origin: `bottom`, interval: 200 })
+sr.reveal(`.home__social,  .footer__create`, { origin: `bottom`, interval: 200 })
 sr.reveal(`.about__data`, { origin: `left`, })
 sr.reveal(`.sec__data`, { origin: `right`, })
